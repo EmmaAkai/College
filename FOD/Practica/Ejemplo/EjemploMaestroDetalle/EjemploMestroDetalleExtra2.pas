ac1d14cb94515241156a64d9193ee2538edd8707
Emma Akai <EmmaAkaiGit@gmail.com>
2024-05-28 17:37:41 -0300

    gracias a juanmita pue hacer el e1p2 jijiji

--- FOD/Practica/Ejemplo/EjemploMaestroDetalle/EjemploMestroDetalleExtra2.pas
+++ FOD/Practica/Ejemplo/EjemploMaestroDetalle/EjemploMestroDetalleExtra2.pas
@@ -1,23 +1,27 @@
 procedure leer (var archivo:detalle; var dato:v_prod);
 begin
-if (not eof(archivo))
-then read (archivo,dato)
-else dato.cod := valoralto;
-end;
+    if (not eof(archivo)) then 
+        read (archivo,dato)
+    else 
+        dato.cod := valoralto;
+    end;
+
 begin
-assign (mae1, 'maestro'); assign (det1, 'detalle');
-reset (mae1); reset (det1);
-leer(det1,regd); {se procesan todos los registros del archivo det1}
-while (regd.cod <> valoralto) do begin
-read(mae1, regm);
-while (regm.cod <> regd.cod) do
-read (mae1,regm);
-{ se procesan códigos iguales }
-while (regm.cod = regd.cod) do begin
-regm.cant := regm.cant - regd.cv;
-leer(det1,regd);
-end;
-{reubica el puntero}
-seek (mae1, filepos(mae1)-1);
-write(mae1,regm);
-end;
+    assign (mae1, 'maestro'); assign (det1, 'detalle');
+    reset (mae1); reset (det1);
+    leer(det1,regd); {se procesan todos los registros del archivo det1}
+
+    while (regd.cod <> valoralto) do begin
+        read(mae1, regm);
+        while (regm.cod <> regd.cod) do
+            read (mae1,regm);
+        { se procesan códigos iguales }
+        while (regm.cod = regd.cod) do begin
+            regm.cant := regm.cant - regd.cv;
+            leer(det1,regd);
+        end;
+        {reubica el puntero}
+        seek (mae1, filepos(mae1)-1);
+        write(mae1,regm);
+    end;
+end.
--- FOD/Practica/Practica 2/e1p2/e1p2.pas
+++ FOD/Practica/Practica 2/e1p2/e1p2.pas
@@ -1,4 +1,16 @@
+{Una empresa posee un archivo con información de los ingresos percibidos por diferentes
+empleados en concepto de comisión, de cada uno de ellos se conoce: código de empleado,
+nombre y monto de la comisión. La información del archivo se encuentra ordenada por
+código de empleado y cada empleado puede aparecer más de una vez en el archivo de
+comisiones.
+Realice un procedimiento que reciba el archivo anteriormente descrito y lo compacte. En
+consecuencia, deberá generar un nuevo archivo en el cual, cada empleado aparezca una
+única vez con el valor total de sus comisiones.
+NOTA: No se conoce a priori la cantidad de empleados. Además, el archivo debe ser
+recorrido una única vez.}
 program e1p2;
+const
+    valorAlto=999;
 
 type
     Empleado = Record
@@ -7,16 +19,54 @@ type
         monto:real;
     end;
 
-    EmpleadoFile = file of Empleado;
+    EmpleadoNuevo = Record
+        cod:integer;
+        nombre:string[18];
+        montoTot:real;
+    end;
 
-procedure importar(var EFile: EmpleadoFile);
-    var
+    EmpleadoFile = file of Empleado;
+    NuevoFile = file of EmpleadoNuevo;
+
+procedure leer(var archivo: EmpleadoFile; var e: Empleado);
     begin
-        
+        if(not eof(archivo)) then
+            read(archivo, e);
+        else
+            e.cod:=valorAlto;
+    end;
+
+procedure crear(var nFile:NuevoFile; var eFile:EmpleadoFile);
+    var
+        e:Empleado;
+        n:EmpleadoNuevo;
+        codAct:integer;
+        tot:integer;
+    begin
+        rewrite(nFile);
+        reset(eFile);
+        leer(eFile,e)
+        while(e.cod <> ValorAlto) do begin
+            codAct:=e.cod;
+            tot:=0;
+            while(codAct:=e.cod) do begin
+                tot:=tot+e.monto;
+                leer(eFile,e);
+            end;
+            n.cod:=e.cod;
+            n.nombre:=e.nombre;
+            n.montoTot:=tot;
+            write(nFile,n);
+        end;
+        close(nFile);
+        close(eFile);
     end;
 
 var
-    EFile: EmpleadoFile;
+    eFile: EmpleadoFile;
+    nFile: NuevoFile;
 begin
-    
+    assign(eFile,'Empleados'); //se dispone
+    assign(nFile,'NuevosEmp');
+    crear(nFile,eFile);
 end.
